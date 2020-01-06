127dab3eae043659bcf9172ffa8e43058bbeff2a
SamTheCoder777 <49662629+SamTheCoder777@users.noreply.github.com>
2020-01-05 20:21:48 -0500

    Update HwThree.cpp

--- HwThree.cpp
+++ HwThree.cpp
@@ -2,43 +2,27 @@
 #include <math.h>
 using namespace std;
 int main()
-{
-	int n, num, digit, rev = 0;
-	int x;
-	cout << "Enter a positive number: ";
-	cin >> x;
-	int i = x;
-	num = x;
-	n = num;
-	do
-	{
-		digit = num % 10;
-		rev = (rev * 10) + digit;
-		num = num / 10;
-	} while (num != 0);
-	cout << " The reverse of the number is: " << rev << endl;
-	if (n == rev)
-		cout << " The number is a palindrome.";
+{	
+	string palindrome="";
+	string text;
+	cin >> text;
+	int length = text.size();
+	text[length];
+
+	for (int x = length-1; x >= 0; x--) {
+		palindrome += text[x];
+	}
+	if (palindrome == text) {
+		cout << "It is palindrome";
+	}
 	else {
-		int revs = 0;
-		int number = i;
-		int a = num;
-		int digits;
-		int newNumber;
-		for (int x = 1; x <= digit; x++) {
-			number = number - pow(10, x);
-			do
-			{
-				digits = number % 10;
-				rev = (rev * 10) + digit;
-				num = num / 10;
-			} while (num != 0);
-			if (n == rev) {
-				cout << "The number is a palindrome";
+		string palindromeA="";
+		for (int a = 0; a <= length; a++) {
+			palindromeA = palindrome[length] - palindrome[a];
+			if (palindromeA == text) {
+				cout << "It is a conditional palindrome";
 				break;
 			}
-			else
-				continue;
 		}
 	}
 	return 0;
